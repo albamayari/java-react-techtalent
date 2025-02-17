31e5189abc5f2963315fd13965f0805b35b9afb1
Alba Ma <alba.maydeu@gmail.com>
2025-02-17 12:59:43 +0100

    Orden

--- Github - Eclipse/c2-javabasics/src/ejercicios/ejercicio1.java
+++
@@ -1,9 +0,0 @@
-package ejercicios;
-
-public class ejercicio1 {
-
-	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
-	}
-
-}
--- c2-javabasics/src/ejercicios/Ejercicio1App.java
+++ c2-javabasics/src/ejercicios/Ejercicio1App.java
@@ -3,7 +3,8 @@ package ejercicios;
 public class Ejercicio1App {
 
 	public static void main(String[] args) {
-		//fcfj
+		
+		System.out.println("Prueba");
 
 	}
 
