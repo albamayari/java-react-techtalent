1d1e3953528d7a81c5eca82c6c803ba6eb06efe9
Alba Ma <alba.maydeu@gmail.com>
2025-02-17 11:56:44 +0100

    jg

---
+++ Github - Eclipse/c2-javabasics/src/ejercicios/ejercicio1.java
@@ -0,0 +1,9 @@
+package ejercicios;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		System.out.println("HolaMundo");
+	}
+
+}
---
+++ c2-javabasics/src/ejercicios/Ejercicio1App.java
@@ -0,0 +1,10 @@
+package ejercicios;
+
+public class Ejercicio1App {
+
+	public static void main(String[] args) {
+		//fcfj
+
+	}
+
+}
